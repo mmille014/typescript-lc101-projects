b99ff268b055656bce4626b2dd1d719685194f97
Miller <MMille014@cable.comcast.com>
2020-04-15 15:57:30 -0400

    typeScript Studio Completed

---
+++ studio/classes/Astronaut.ts
@@ -0,0 +1,14 @@
+import { Payload } from './../Payload';
+import { Cargo } from './Cargo';
+import {Rocket} from './Rocket';
+
+export class Astronaut implements Payload {
+    massKg: number;
+    name: string
+    constructor(massKg: number, name: string) {
+        this.massKg = massKg;
+        this.name = name;
+    }
+
+ }
+
